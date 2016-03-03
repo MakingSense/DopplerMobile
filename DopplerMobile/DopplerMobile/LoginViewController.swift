3b24cc650073a5f50ba7da84c874d6dcb35aa09d
Julian Waimann <jwaimann@makingsense.com>
2016-03-03 09:29:43 -0300

    Implemented main view with logout button.

---
+++ DopplerMobile/DopplerMobile/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  DopplerMobile
+//
+//  Created by Julian Waimann on 2/24/16.
+//  Copyright © 2016 Making Sense. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true;
+    }
+
+}
+
