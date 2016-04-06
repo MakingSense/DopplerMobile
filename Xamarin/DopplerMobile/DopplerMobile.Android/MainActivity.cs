05db81d81ac26d93a42af82f8310fdf37884e69b
Marcelo Vazquez <mvazquez@makingsense.com>
2016-04-06 17:17:08 -0300

    Moved native iOS project to a separated folder. Created one for Xamarin projects with an initial structure.

---
+++ Xamarin/DopplerMobile/DopplerMobile.Android/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace DopplerMobile.Android
+{
+    [Activity(Label = "DopplerMobile.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int _count = 1;
+        private Button _button;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            _button = FindViewById<Button>(Resource.Id.MyButton);
+            _button.Click += ButtonOnClick;
+        }
+
+        private void ButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            _button.Text = $"{_count++} clicks!";
+        }
+
+        protected override void OnDestroy()
+        {
+            _button.Click -= ButtonOnClick;
+            base.OnDestroy();
+        }
+    }
+}
+
---
+++ Xamarin/DopplerMobile/DopplerMobile.iOS.UnitTest/AppDelegate.cs
@@ -0,0 +1,41 @@
+﻿using Foundation;
+using UIKit;
+using MonoTouch.NUnit.UI;
+
+namespace DopplerMobile.iOS.UnitTest
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        // class-level declarations
+        UIWindow _window;
+        TouchRunner _runner;
+
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            // create a new window instance based on the screen size
+            _window = new UIWindow(UIScreen.MainScreen.Bounds);
+            _runner = new TouchRunner(_window);
+
+            // register every tests included in the main application/assembly
+            _runner.Add(System.Reflection.Assembly.GetExecutingAssembly());
+
+            _window.RootViewController = new UINavigationController(_runner.GetViewController());
+
+            // make the window visible
+            _window.MakeKeyAndVisible();
+
+            return true;
+        }
+    }
+}
