e01fb268b4ebe59177b51f7c464f7ead804204c6
Mauro Maldini <maldini.mauro@gmail.com>
2016-04-25 16:50:46 -0300

    [DM-4] Added visibility plugin to fix ProgresBar bug

---
+++ Xamarin/DopplerMobile/DopplerMobile.Android/Bootstrap/VisibilityPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace DopplerMobile.Android.Bootstrap
+{
+    public class VisibilityPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.Visibility.PluginLoader>
+    {
+    }
+}
