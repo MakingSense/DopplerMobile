e64f19089dac15ba368990bfe0c839451e7b78be
Julian Waimann <jwaimann@makingsense.com>
2016-06-22 14:01:45 -0300

    Implement Login View and ViewController

---
+++ iOS/DopplerMobile/DopplerMobile/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  DopplerMobile
+//
+//  Created by Julian Waimann on 2/24/16.
+//  Copyright © 2016 Making Sense. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController
+{
+
+    //MARK: Properties
+
+    @IBOutlet weak var txtUsername: UITextField!
+
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    
+    //MARK: Login Button's Actions
+    
+  
+    @IBAction func Login(sender: UIButton)
+    {
+        //TODO: pending implementation
+    }
+    
+    
+    //MARK: Forgot Password Button's Actions
+    
+    @IBAction func ForgotPassword(sender: UIButton)
+    {
+        //TODO: pending implementation
+    }
+}
+
