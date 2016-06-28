4944632470773b10eded06d0305beb0ac7b0a1f6
Mugu <jmuguruza@makingsense.com>
2016-06-28 16:41:37 -0300

    Removed protocol for OnboardingViewModel, also added a separated OnboardingContentViewModel.

--- iOS/DopplerMobile/DopplerMobile/Application/ViewModels/OnboardingViewModel.swift
+++ iOS/DopplerMobile/DopplerMobile/Application/ViewModels/OnboardingViewModel.swift
@@ -8,41 +8,48 @@
 
 import Foundation
 
-protocol OnboardingViewModelDelegate: class
-{
-    func setupOnboardingContent()
-
-    func next() -> String
-
-    func skip()
-}
-
 public class OnboardingViewModel
 {
     //As content is an array of strings I tried to use a plural name but "contents" just plain sucks. I left content.
-    var content : [String] = []
+    var content : [OnboardingContentViewModel] = []
     var currentIndex : Int = 0
-
-    weak var delegate: OnboardingViewModelDelegate?
 
     func setupOnboardingContent()
     {
         //TODO: Get actual content from a localization solution.
-        self.content = ["One", "Two", "Three"];
+        addTestContent()
     }
 
-    func next() -> String
+    func next() -> OnboardingContentViewModel?
     {
-        self.currentIndex += 1
-        let contentToReturn = self.content[self.currentIndex]
-
+        var contentToReturn : OnboardingContentViewModel?
+        
+        if(content.count > self.currentIndex)
+        {
+            self.currentIndex += 1
+            contentToReturn = self.content[self.currentIndex]
+            
+        }
+        //maybe we should handle the else? with an error or something [Mugu]
+        
         return contentToReturn
     }
 
     func skip()
     {
-        //What is this supposed to return? [Mugu]
+        //TODO: Check back later when we decide if the VM or the VC are gonna take care of the app navigation.
     }
 
-
+    //TODO: Remove this later, only for testing.
+    func addTestContent()
+    {
+        let arrayOfStrings : [String] = ["One", "Two", "Three"];
+        
+        for string in arrayOfStrings
+        {
+            let newContent = OnboardingContentViewModel()
+            newContent.setup(string)
+            self.content.append(newContent)
+        }
+    }
 }
