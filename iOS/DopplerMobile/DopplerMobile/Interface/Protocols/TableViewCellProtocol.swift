aafcc036766c6b61841e34f0468b2d2aec36913a
Mauro Maldini <mmaldini@makingsense.com>
2016-09-30 15:03:24 -0300

    [NoTicket] Code Refactor: working on project structure.

---
+++ iOS/DopplerMobile/DopplerMobile/Interface/Protocols/TableViewCellProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCellProtocol.swift
+//  DopplerMobile
+//
+//  Created by Mauro Maldini on 30/9/16.
+//  Copyright © 2016 Making Sense. All rights reserved.
+//
+
+import Foundation
+
+protocol TableViewCellProtocol: class
+{
+    func configure<T>(viewModel: T)
+}
+
+
+
