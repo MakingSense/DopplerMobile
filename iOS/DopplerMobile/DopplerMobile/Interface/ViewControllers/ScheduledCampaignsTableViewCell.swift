724febb79c80d1c01e81ec06b09f896498127f53
Juan Sebastian Muguruza <jmuguruza@makingsense.com>
2016-10-28 13:00:54 -0300

    [DM-248] Scheduled Campaigns :: Subtitle needs fallback content and actual date.

--- iOS/DopplerMobile/DopplerMobile/Interface/ViewControllers/ScheduledCampaignsTableViewCell.swift
+++ iOS/DopplerMobile/DopplerMobile/Interface/ViewControllers/ScheduledCampaignsTableViewCell.swift
@@ -21,13 +21,35 @@ class ScheduledCampaignsTableViewCell : UITableViewCell
     func configure(_ campaignViewModel: CampaignViewModel)
     {
         self.lblCampaignName.text = campaignViewModel.name
-        self.lblCampaignDate.text = campaignViewModel.sentDate?.toStringWithFormat(DateFormatEnum.yyyy_MM_dd.pattern)
-        //TODO: get and show the right information.
+        self.lblCampaignDate.attributedText = prepare(date: campaignViewModel.sentDate, subscribersAmount: campaignViewModel.amountSentSubscribers!)
         let lists = campaignViewModel.amountSentSubscribers!
         let listsText = lists == 1 ? "\("REPORTS_LIST".localized)" : "\("REPORTS_LISTS".localized)"
         self.lblCampaignStatistics.text = "\(lists) \(listsText)"
         let subscribers = campaignViewModel.amountSentSubscribers!
         let subscribersText = subscribers == 1 ? "\("REPORTS_SUBSCRIBER".localized)" : "\("REPORTS_SUBSCRIBERS".localized)"
         self.lblStatisticsRight.text = "\(subscribers) \(subscribersText)"
+    }
+    
+    func prepare(date: Date?, subscribersAmount: Int) -> NSMutableAttributedString
+    {
+        var dateString : String
+        
+        if(date != nil)
+        {
+            dateString = "SCHEDULED_CAMPAIGN_DATE_TO".localized
+            dateString = dateString.replacingOccurrences(of: "{date}", with: (date?.toStringWithFormat(DateFormatEnum.yyyy_MM_dd.pattern))!)
+        }
+        else
+        {
+            dateString = "SCHEDULED_CAMPAIGN_NO_DATE".localized
+        }
+        
+        var subscribers = "SCHEDULED_CAMPAIGN_AMOUNT_SUBSCRIBERS".localized
+        subscribers = subscribers.replacingOccurrences(of: "{subscribersAmount}", with: String(subscribersAmount))
+        
+        let formattedString = NSMutableAttributedString()
+        formattedString.append(text: dateString).append(text: subscribers, font: UIFont.boldOf(size: 13.0))
+        
+        return formattedString
     }
 }
