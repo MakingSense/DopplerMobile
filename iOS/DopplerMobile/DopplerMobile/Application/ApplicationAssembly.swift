f525ec2e8e5d6d0adf70bda2680d617b7c4c47da
mauro maldini <maldini.mauro@gmail.com>
2017-05-30 16:57:46 -0300

    Translate ForgotPassword Texts.
    Minimal localizacion of forgot passwords strings.

--- iOS/DopplerMobile/DopplerMobile/Application/ApplicationAssembly.swift
+++ iOS/DopplerMobile/DopplerMobile/Application/ApplicationAssembly.swift
@@ -24,11 +24,15 @@ public class ApplicationAssembly : TyphoonAssembly
     
     public dynamic func loginViewModel() -> Any { return DI.lazySingleton(LoginViewModel.self, selector: #selector(LoginViewModel.init(authenticationService:navigationDelegate:)), initParameters: [self.authenticationService(), self.navigationDelegate()])}
     
+    public dynamic func forgotPasswordViewModel() -> Any { return DI.lazySingleton(ForgotPasswordViewModel.self, selector: #selector(ForgotPasswordViewModel.init(authenticationService:navigationDelegate:)), initParameters: [self.authenticationService(), self.navigationDelegate()])}
+    
     public dynamic func accountViewModel() -> Any { return DI.lazySingleton(AccountViewModel.self, selector: #selector(AccountViewModel.init(authenticationService:navigationDelegate:)), initParameters: [self.authenticationService(), self.navigationDelegate()])}
 
     // MARK: ViewControllers
     
     public dynamic func loginViewController() -> Any { return DI.defaultWithProperties(LoginViewController.self, properties: ["viewModel" : self.loginViewModel()])}
+    
+    public dynamic func forgotPasswordViewController() -> Any { return DI.defaultWithProperties(ForgotPasswordViewController.self, properties: ["viewModel" : self.forgotPasswordViewModel()])}
 
     public dynamic func accountViewController() -> Any { return DI.defaultWithProperties(AccountViewController.self, properties: ["viewModel" : self.accountViewModel()])}
 }
